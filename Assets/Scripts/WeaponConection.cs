eda56a81fe3319caba90c87ef518fdd90e134f35
Joymg <lucia.ortuno22@gmail.com>
2024-07-20 19:34:26 +0200

    Weapon conection visuals

---
+++ Assets/Scripts/WeaponConection.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WeaponConection : MonoBehaviour
+{
+    [SerializeField] private List<BoatController> _boatsToConnect;
+    [SerializeField] private SpriteRenderer _tubeSprite;
+
+    private Vector2 _tubeSize;
+    void Start()
+    {
+        if (_tubeSprite == null)
+        {
+            _tubeSprite = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        if (_boatsToConnect.Count < 2)
+        {
+            _boatsToConnect = FindObjectsOfType<BoatController>().ToList();
+        }
+
+        SetTubeTransform();
+    }
+
+    
+    void Update()
+    {
+        SetTubeTransform();
+    }
+
+    private void SetTubeTransform()
+    {
+        _tubeSprite.transform.position = _boatsToConnect[1].transform.position;
+        _tubeSprite.transform.rotation *= Quaternion.FromToRotation(_tubeSprite.transform.right, _boatsToConnect[0].transform.position - _boatsToConnect[1].transform.position);
+        //_tubeSprite.transform.LookAt(_boatsToConnect[0].transform.position);
+        _tubeSize = new Vector2(Vector3.Magnitude(_boatsToConnect[1].transform.position - _boatsToConnect[0].transform.position), _tubeSprite.size.y);
+        _tubeSprite.size = _tubeSize;
+    }
+}
